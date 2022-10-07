febb891a045bfb4058de2688782f33fa8cfc0dc2
Le Thi Ngoc Lan <lnlan1810@gmail.com>
2022-10-07 18:18:43 +0300

    first commit

---
+++ week_5/ServerSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpServer
+{
+    public class ServerSetting
+    {
+        public int Port { get; set; } = 7700;
+
+        public string Path { get; set; } = @"./site/";
+    }
+}
